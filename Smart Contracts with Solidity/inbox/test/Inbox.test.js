7fbd562f0ee2a21885b75f7ef8da3465d47469ac
Developers@Work <DevelopersWork@users.noreply.github.com>
2024-03-03 23:47:05 +0530

    53. Testing Message Updates

--- Smart Contracts with Solidity/inbox/test/Inbox.test.js
+++ Smart Contracts with Solidity/inbox/test/Inbox.test.js
@@ -3,6 +3,8 @@ const ganache = require('ganache');
 const { Web3 } = require('web3');
 const { interface, bytecode } = require('../compile').contracts[':Inbox'];
 
+const INITIAL_MESSAGE = 'Hello World!';
+const UPDATE_MESSAGE = 'Hello Universe!';
 
 const web3 = new Web3(ganache.provider());
 
@@ -15,7 +17,7 @@ beforeEach(async () => {
     // Use one of those accounts to deploy
     // the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hello World!'] })
+        .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
         .send({ from: accounts[0], gas: '1000000' });
 });
 
@@ -23,4 +25,15 @@ describe('Inbox', () => {
     it('check contract deployment', () => {
         assert.ok(inbox.options.address);
     });
+
+    it('check initial message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_MESSAGE);
+    });
+
+    it('check update message', async () => {
+        await inbox.methods.setMessage(UPDATE_MESSAGE).send({ from: accounts[1] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, UPDATE_MESSAGE);
+    })
 });
