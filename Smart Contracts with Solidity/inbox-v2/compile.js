3a56a0c450d5c06db91ce2af54033f81ca6f6c4d
Developers@Work <DevelopersWork@users.noreply.github.com>
2024-03-05 14:17:38 +0530

    63. Updating Your Inbox Project to Solc v0.8.9

---
+++ Smart Contracts with Solidity/inbox-v2/compile.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf-8');
+
+const Input = {
+    language: 'Solidity',
+    sources: {
+        'Inbox.sol': {
+            content: source
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*']
+            }
+        }
+    }
+}
+
+const json_output = JSON.parse(solc.compile(JSON.stringify(Input)));
+module.exports = json_output.contracts;
