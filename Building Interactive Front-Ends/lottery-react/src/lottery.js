247b3fabf0dbd69e9ff8bf6b3b32a90d29fc4276
Developers@Work <DevelopersWork@users.noreply.github.com>
2024-03-13 23:50:54 +0530

    103. Local Contract Instances

---
+++ Building Interactive Front-Ends/lottery-react/src/lottery.js
@@ -0,0 +1,7 @@
+import web3 from "./web3";
+
+const CONTRACT_ADDRESS = '0xf99a77EB209935C8aaDAd16aC3309d59480B5f6C';
+const ABI = [{ "constant": true, "inputs": [], "name": "manager", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "pickWinner", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "getPlayers", "outputs": [{ "name": "", "type": "address[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "enter", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "players", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }]
+
+const contract = new web3.eth.Contract(ABI, CONTRACT_ADDRESS);
+export default contract;
